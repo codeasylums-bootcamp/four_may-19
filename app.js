6a3bd5be4916385a439fe1c0e84a9db75af1b132
Rishabh <rishabhkuril@gmail.com>
2019-05-26 12:30:03 +0530

    adding api

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 function writeIt() {
 
     var textToTranslate = document.getElementById("sourceLang").value;
-    console.log(textToTranslate);
-    
+    //console.log(textToTranslate);
+
+    var oReq = new XMLHttpRequest;
+
+    // oReq.addEventListener('load', function (event) {
+    //     json = JSON.parse(this.responseText);
+    //     console.log(json);
+    // });
+    var el = document.getElementById('writeBtn');
+    el.addEventListener("click", function(event){
+        console.log(this.responseText);
+    });
+
+
+    oReq.open("GET", 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190526T063700Z.f829db6669afabd2.99c804c5fa4006930bd8c6b0ebc17d5c768ab1e8&lang=ru&text='+textToTranslate)
+    /*
+    .then((responseText) =>{
+        console.log(responseText);
+    });
+    */
+    oReq.send();
+
 }
