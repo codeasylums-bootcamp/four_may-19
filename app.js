30c8a1de7a48fe06298c6771b0323a8fdefce401
Rishabh <rishabhkuril@gmail.com>
2019-05-26 13:20:19 +0530

    support for 1 lang

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function writeIt(){
     oReq.open("POST", 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190526T063700Z.f829db6669afabd2.99c804c5fa4006930bd8c6b0ebc17d5c768ab1e8&lang=ru&text='+textToTranslate, true)
     oReq.send();
     oReq.addEventListener('load', function (event) {
-        json = JSON.parse(this.responseText);
+        var json = JSON.parse(this.responseText);
         console.log(json);
     });
 }
